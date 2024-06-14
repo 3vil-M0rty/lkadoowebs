387361440759956b218d1b6532dfc41132148c23
3vil-M0rty <142729021+3vil-M0rty@users.noreply.github.com>
2024-06-14 03:06:08 +0100

    okay

--- static/js/background.js
+++ static/js/background.js
@@ -77,3 +77,22 @@ document.getElementById('prev').addEventListener('click', () => {
         slider.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const backToTopButton = document.querySelector(".back-to-top");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    backToTopButton.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
